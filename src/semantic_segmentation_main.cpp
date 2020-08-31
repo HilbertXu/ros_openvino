559738af0d9501ac11ac5a953a9ff541f2576f1e
HilbertXu <hilbertxu@outlook.com>
2020-08-31 23:35:24 +0800

    Update 2020/08/31 add maskrcnn entry point

---
+++ src/semantic_segmentation_main.cpp
@@ -0,0 +1,20 @@
+/*
+ * semantic_segmentation_main.cpp
+ * 
+ *  Created on: July 31th, 2020
+ *      Author: Hilbert Xu
+ *   Institute: Mustar Robot
+ */
+
+#include <robot_vision_openvino/vino_maskrcnn/maskrcnn_ros.hpp>
+
+int main(int argc, char** argv) {
+	ros::init (argc, argv, "maskrcnn_ros");
+	ros::NodeHandle nodeHandle_("~");
+	semantic_segmentation_maskrcnn::MaskrcnnROS segmentation(nodeHandle_);
+
+	ros::spin();
+	return 0;
+}
+ 
+ 
