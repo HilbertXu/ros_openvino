dfd55cd8700855b0fc044375e8b6d849254b2a3c
HilbertXu <hilbertxu@outlook.com>
2020-08-31 23:26:19 +0800

    Update 2020/08/31 add maskrcnn includes

--- include/robot_vision_openvino/vino_maskrcnn/vino_maskrcnn.hpp
+++ include/robot_vision_openvino/vino_maskrcnn/vino_maskrcnn.hpp
@@ -36,6 +36,7 @@
 #include <image_transport/image_transport.h>
 
 // OpenCV
+#include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -44,6 +45,9 @@
 #include <robot_control_msgs/Mission.h>
 #include <robot_control_msgs/Results.h>
 #include <robot_control_msgs/Feedback.h>
+#include <robot_vision_msgs/BoundingBoxes.h>
+#include <actionlib/server/simple_action_server.h>
+#include <robot_vision_msgs/CheckForObjectsAction.h>
 
 // OpenVINO engine
 #include <ngraph/ngraph.hpp>
@@ -55,21 +59,49 @@
 
 using namespace InferenceEngine;
 
-namespace semantic_segment_maskrcnn {
+namespace semantic_segmentation_maskrcnn {
+
+  struct MaskBox {
+		// 结构体成员包括
+		// 成员变量：
+		// bounding_box的四个角的像素坐标，物体类别，置信度
+		int xmin, ymin, xmax, ymax, box_height, box_width, class_id;
+		float confidence;
+  };
+
+  // MaskRCNN class
   class MaskRCNN {
+  public:
+    size_t netBatchSize;
+    size_t netInputHeight;
+    size_t netInputWidth;
     std::string targetDeviceName_;
     std::string detectionOutputName_;
     std::string maskName_;
+    InferenceEngine::CNNNetwork network;
     InferenceEngine::Core inferenceEngine_;
     InferenceEngine::ExecutableNetwork executableNetwork;
-    // MaskRCNN class
-  public:
+    InferenceEngine::InferRequest::Ptr inferRequestCurr;
     // 默认构造函数
+    MaskRCNN();
     MaskRCNN(std::string targetDeviceName, std::string detectionOutputName, std::string maskName);
     // 默认析构函数
     ~MaskRCNN();
 
     // 加载网络权重文件与网络结构文件，并配置网络的输入输出
     void setUpNetwork(std::string modelPath_);
-  }
+
+    // 将图像帧转换为模型输入的blob数据
+    void frameToBlob(const cv::Mat &frame);
+
+    // 开始当前图像帧的推理
+    void startCurr();
+
+    // 判断当前图像帧的推理是否结束
+    bool readyCurr();
+
+    // 处理视频帧的识别结果
+    cv::Mat postProcessCurr(const cv::Mat &frame, std::vector<MaskBox> &objects);
+
+  };
 }
